81566b19c0c2c85cc60f5ec35bec39f87b76f522
HOARAU <Mathieuhoarau@laposte.net>
2021-10-20 09:06:29 +0200

    affichage d'image

--- screens/ImagesScreen.js
+++ screens/ImagesScreen.js
@@ -1,7 +1,47 @@
-import React from "react";
-import {Text} from "react-native";
+import * as FileSystem from "expo-file-system";
 
-const ImagesScreen = ()=> {
-    return <Text>Images Screen</Text>
-};
-export default ImagesScreen;
+import React, { useEffect, useState } from "react";
+
+import { StyleSheet, Image, FlatList } from "react-native";
+
+
+export default function ImagesScreen() {
+
+  const [image, setImage] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const images = await FileSystem.readDirectoryAsync(
+        FileSystem.cacheDirectory + "ImageManipulator"
+      );
+      setImage(images);
+    })();
+  }, []);
+
+  return image.length > 0 ? (
+
+    <FlatList
+      data={image}
+      keyExtractor={(imageURI) => imageURI}
+      renderItem={(itemData) => {
+        console.log("item", itemData);
+        return (
+          <Image
+            style={styles.image}
+            source={{
+              uri:
+                FileSystem.cacheDirectory + "ImageManipulator/" + itemData.item,
+            }}
+          />
+        );
+      }}
+    />
+  ) : null;
+}
+
+const styles = StyleSheet.create({
+  image: {
+    resizeMode: "cover",
+    height: 500,
+  },
+});
